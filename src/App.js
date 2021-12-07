6ed0de72d0d39ab73e604f31f955e3a8a6a0742c
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-07 15:17:59 +0500

    Get method (axios)

--- src/App.js
+++ src/App.js
@@ -1,52 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
+class App extends React.Component {
+  state = {
+    posts: [],
+  };
 
-function App() {
-  return (
-    <div className="container p-5">
-      <div className="row">
-        <div className="col">
-          <button className="btn btn-primary">
-        Add
-          </button>
-          <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Update</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {this.state.posts.map(post => (
-            <tr key={post.id}>
-              <td>{post.title}</td>
-              <td>
-                <button
-                  className="btn btn-info btn-sm"
-                  onClick={() => this.handleUpdate(post)}
-                >
-                  Update
-                </button>
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => this.handleDelete(post)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))} */}
-        </tbody>
-          </table>
+  async componentDidMount() {
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    // console.log(posts);
+    this.setState({ posts });
+  }
+
+  handleAdd = () => {
+    console.log("add");
+  };
+
+  handleUpdate = (post) => {
+    console.log("update", this.state.posts);
+  };
+  handleDelete = (post) => {
+    console.log("delete", this.state.posts);
+  };
+
+  render() {
+    return (
+      <div className="container p-5">
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-primary" onClick={this.handleAdd}>
+              Add
+            </button>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Update</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.posts.map((post) => (
+                  <tr key={post.id}>
+                    <td>{post.title}</td>
+                    <td>
+                      <button
+                        className="btn btn-info btn-sm"
+                        onClick={() => this.handleUpdate(post)}
+                      >
+                        Update
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => this.handleDelete(post)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
