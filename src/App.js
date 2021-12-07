dd07b83647cfa8d8e10d0940f27954d7f14dec3e
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-07 14:16:14 +0500

    starter prjct for hndling backend srvices in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-primary">
+        Add
+          </button>
+          <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {this.state.posts.map(post => (
+            <tr key={post.id}>
+              <td>{post.title}</td>
+              <td>
+                <button
+                  className="btn btn-info btn-sm"
+                  onClick={() => this.handleUpdate(post)}
+                >
+                  Update
+                </button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => this.handleDelete(post)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))} */}
+        </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
